0a5e737957ef9dba547f41b461cef053e4f27aed
Atharva Kulkarni <atharvak56@gmail.com>
2020-07-26 22:27:54 -0500

    changes

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -39,9 +39,6 @@ function modeSwitcher() {
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }
-$(document).ready(function () {
-	modeSwitcher();
-  });
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
